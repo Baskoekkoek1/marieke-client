7c7253b6bd752f25becd1bb02ab3ce8d3747409c
Baskoekkoek1 <baskoekkoek@gmail.com>
2021-02-16 16:59:20 +0100

    add voltInfo

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import styles from "./index.module.scss";
 import Header from "./Components/Header";
 import MainContent from "./Components/MainContent";
 import Footer from "./Components/Footer";
+import VoltInfo from "./Components/VoltInfo";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContent />
+      <VoltInfo />
       <Footer />
     </div>
   );
