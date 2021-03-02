1fc871c7a6fd2e6fc343ed13202f34f285e93693
Baskoekkoek1 <baskoekkoek@gmail.com>
2021-03-02 15:29:28 +0100

    add links to social

--- src/Components/Social/index.tsx
+++ src/Components/Social/index.tsx
@@ -6,27 +6,28 @@ export default function Social() {
     <div>
       <ul className="social">
         <li>
-          <a href="#" title="">
+          <a href="https://twitter.com/koekkoekmarieke?lang=en" title="twitter">
             <span className="icon fa fa-twitter"></span>
           </a>
         </li>
         <li>
-          <a href="#" title="">
+          <a href="https://www.facebook.com/marieke.koekkoek/" title="facebook">
             <span className="icon fa fa-facebook"></span>
           </a>
         </li>
         <li>
-          <a href="#" title="">
+          <a
+            href="https://www.linkedin.com/in/marieke-koekkoek/"
+            title="linkedin"
+          >
             <span className="icon fa fa-linkedin"></span>
           </a>
         </li>
         <li>
-          <a href="#" title="">
-            <span className="icon fa fa-youtube"></span>
-          </a>
-        </li>
-        <li>
-          <a href="#" title="">
+          <a
+            href="https://www.instagram.com/marieke_koekkoek/"
+            title="instagram"
+          >
             <span className="icon fa fa-instagram"></span>
           </a>
         </li>
